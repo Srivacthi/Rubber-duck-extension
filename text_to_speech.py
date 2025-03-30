7ab78bf70c78b94432f94f2932106414bd53ca20
Srivacthi <Srivacthis@outlook.com>
2025-03-30 05:31:06 -0400

    added context history, persiting on mongodb and looping conversation

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,6 +3,11 @@ import os
 import speech_recognition as sr
 import requests
 import json
+import time
+from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
+from playsound import playsound
+
+TXT_FILE = "conversation_history.txt"
 
 def text_to_speech(text):
     tts = gTTS(text=text, lang="en")
@@ -11,13 +16,11 @@ def text_to_speech(text):
 
 def speech_to_text(): #returns your question to text
     r = sr.Recognizer()
-
     with sr.Microphone(device_index=1) as source:
         print("Listening...")
         # reducing background noice
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-
     try:
         text = r.recognize_google(audio)  # Convert speech to text
         print("You said:", text)
@@ -29,7 +32,6 @@ def speech_to_text(): #returns your question to text
 
 def ask_gemini(prompt):
     GEMINI_API_KEY = "AIzaSyD6auRHH1vSuds0MCtvfvbsJ-rocubWfQs"
-
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
     headers = {
         "Content-Type": "application/json"
@@ -48,10 +50,45 @@ def ask_gemini(prompt):
     else:
         print("API Request Failed:", response.status_code, response.text)
 
-question=speech_to_text()
-system_prompt = '''You are a helpful AI assistant who is really good at coding and walks the user through any problem.
-You will provide hints and context in the right direction instead of the full answer.
-After the user reaches the answer, you will confirm the answer. The following is the user question:'''
-prompt = system_prompt + question
-generated_text = ask_gemini(prompt)
-text_to_speech(generated_text)
+def add_to_txt(text):
+    with open(TXT_FILE, "a", encoding="utf-8") as file:
+        file.write(text + "\n")
+
+def clear_history():
+    if os.path.exists(TXT_FILE):
+        os.remove(TXT_FILE)
+
+def main():
+    clear_history()
+    chat_with_history = MongoDBChatMessageHistory(
+        session_id="Srivacthi",
+        connection_string="mongodb+srv://Srivacthi:7TahlSkPE0uZdAT6@cluster0.aol7ngg.mongodb.net/?retryWrites=true&w=majority&tls=true",
+        database_name="langchain",
+        collection_name="blank"
+    )
+    chat_with_history.clear()
+    print("What is your question? Say 'quit' to quit.")
+    question = speech_to_text()
+    while (question.lower()!="quit"):
+        add_to_txt("User: " + question)
+        #password = "7TahlSkPE0uZdAT6"
+        context = chat_with_history.messages
+        system_prompt = '''You are a helpful AI assistant who is really good at coding and walks the user through any problem.
+        You will provide hints and context in the right direction instead of the full answer.
+        You will keep your responses direct and on the shorter side.
+        After the user reaches the answer, you will confirm the answer. The following is the user question:'''
+        prompt = system_prompt + question + f"Here is the context: {context}"
+        chat_with_history.add_user_message("User: " + question)
+        generated_text = ask_gemini(prompt)
+        chat_with_history.add_ai_message("AI Response: " + generated_text)
+        add_to_txt("AI response: " + generated_text)
+        text_to_speech(generated_text)
+        #playsound("output3.mp3")
+        time.sleep(5)
+        print("Say 'quit' to quit.")
+        question = speech_to_text()
+
+    chat_with_history.clear()
+
+if __name__ == "__main__":
+    main()
